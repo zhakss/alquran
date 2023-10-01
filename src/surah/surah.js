61bbd94b5443cdf5957477061c0aec19a204346f
zhakss <144654889+zhakss@users.noreply.github.com>
2023-10-01 13:14:19 +0700

    Add files via upload

---
+++ src/surah/surah.js
@@ -0,0 +1,42 @@
+function getURL(e) {
+  const pageURL = window.location.search.substring(1);
+  const urlVariable = pageURL.split('&');
+  for (let i = 0; i < urlVariable.length; i++) {
+      const parameterName = urlVariable[i].split('=');
+    if (parameterName[0] == e) {
+      return parameterName[1];
+    }
+  }
+}
+let id = getURL('id');
+console.log(id);
+let main = document.querySelector('.main');
+let judul = document.querySelector('.title');
+let url = `https://equran.id/api/surat/${id}`;
+async function data() {
+  let res = await fetch(url);
+  let data = await res.json();
+  let title = '';
+  let surah = '';
+  for(let i = 0; i < data.ayat.length; i++){
+  surah += `
+  <div class="col">
+    <div class="ar">${data.ayat[i].ar}</div>
+    <div>${data.ayat[i].tr}</div>
+    <div>${data.ayat[i].idn}</div>
+  </div>
+  `;
+  };
+  title += `
+  <div class="col">
+    <div class="title">${data.nomor}. ${data.nama}</div>
+    <div>${data.nama_latin}</div>
+    <div>${data.arti}</div>
+  </div>
+  `;
+  main.innerHTML = surah;
+  judul.innerHTML = title;
+  document.title = `${data.nama_latin}`;
+  console.log(data);
+};
+data();
